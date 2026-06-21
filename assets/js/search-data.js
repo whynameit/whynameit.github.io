01205519a9e43fd05bfb546ad189949eba502923
whynameit <184912386+whynameit@users.noreply.github.com>
2026-06-21 09:14:54 +0000

    Deploying to gh-pages from @ whynameit/whynameit.github.io@6fdfa449e4fe82810cadcdb55c432e242af855f8 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,45 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-",
+    title: "",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-blog",
+          title: "Blog",
+          description: "一个碳基生物所进行的小小熵减",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/index.html";
+          },
+        },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
